25d233e7868bf09fb3d28ac89766b8f072cccc11
sagnik26 <sagnikjohnson26@gmail.com>
2022-12-22 16:44:20 +0530

    new commit

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-const DB = process.env.DATABASE_URI
+const DB = process.env.DATABASE
 
 const connectDB = async () => {
     try {
